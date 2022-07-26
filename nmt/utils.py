e05eb1322cc350b5e739f749b0ef39d50042350d
patricksoga <psoga@nd.edu>
2022-07-26 11:59:09 -0700

    directed graph experiment

--- nmt/utils.py
+++ nmt/utils.py
@@ -208,22 +208,30 @@ class AutomatonPELayer(nn.Module):
         self.embedding_pos_enc = nn.Linear(self.num_states, embed_dim)
         self.pos_initial = nn.Parameter(torch.Tensor(self.num_states, 1), requires_grad=True)
         self.pos_transition = nn.Parameter(torch.Tensor(self.num_states, self.num_states), requires_grad=True)
+
         nn.init.normal_(self.pos_initial)
         nn.init.orthogonal_(self.pos_transition)
 
     def forward(self, sentence_len):
         device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
-        g = dgl.from_networkx(nx.path_graph(sentence_len))
-        mat = g.adjacency_matrix().to_dense().to(device)
+
+        digraph = nx.DiGraph()
+        digraph.add_edges_from([(i, i+1) for i in range(sentence_len)])
+
+        g = dgl.from_networkx(digraph)
+        adj = g.adjacency_matrix().to_dense().to(device)
         z = torch.zeros(self.num_states, g.num_nodes()-1, requires_grad=False, device=device)
+
         vec_init = torch.cat((self.pos_initial, z), dim=1)
         vec_init = vec_init.transpose(1, 0).flatten()
-        kron_prod = torch.kron(mat.reshape(mat.shape[1], mat.shape[0]), self.pos_transition)
+
+        kron_prod = torch.kron(adj.reshape(adj.shape[1], adj.shape[0]), self.pos_transition)
         B = torch.eye(kron_prod.shape[1], device=device) - kron_prod
 
         encs = torch.linalg.solve(B, vec_init)
         stacked_encs = torch.stack(encs.split(self.num_states), dim=1)
         stacked_encs = stacked_encs.transpose(1, 0)
+
         pe = self.embedding_pos_enc(stacked_encs)
 
         return pe
