b71c54a16a05914d050e599235a053a35311db00
Patrick Soga <psoga@psoga.dhcp.nd.edu>
2022-09-19 02:19:03 -0400

    fix

--- nmt/model.py
+++ nmt/model.py
@@ -143,7 +143,7 @@ class Model(nn.Module):
 
         if self.lape_pos:
             self.pos_embedding = ut.get_lape_encoding(self.config['lape_dim'], self.config['max_pos_length'])
-            self.pos_embedding = self.lape_pos(self.pos_embedding)
+            self.pos_embedding = self.lape_emb(self.pos_embedding)
 
         encoder_inputs = self.get_input(src_toks, is_src=True)
         encoder_outputs = self.encoder(encoder_inputs, encoder_mask)
